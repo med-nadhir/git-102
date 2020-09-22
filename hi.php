a68b1958f50a631b1bbe2508e9915b14dfe38ceb
Med Nadhir <nadhir.houissa@gmail.com>
2020-09-22 10:36:55 +0100

    First Commit in master

---
+++ hi.php
@@ -0,0 +1,20 @@
+Hi, my name is Hi.php
+<p>New paragraph</p>
+
+<p>This is another paragraph</p>
+<p>This is the last paragraph</p>
+<p>This is another extra paragraph</p>
+<h2>This is an H2</h2>
+<small>This is a small tag</small>
+
+<h3>This is a h3</h3>
+
+
+<h4>This a h4</h4>
+<h5>This a h5</h5>
+<h6>This a h6</h6>
+<h1>This a h1</h1>
+<p>Im a simple paragraph</p>
+<span>Span 1</span>
+<span>Span 2</span>
+<span>Span 3</span>
